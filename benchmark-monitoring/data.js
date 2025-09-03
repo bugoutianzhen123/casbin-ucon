15f12e196bc9169514cde7d2b395a7f26633775b
github-action-benchmark <github@users.noreply.github.com>
2025-09-03 05:13:53 +0000

    add Benchmark (go) benchmark result for 295ee996ce9e417ce7debd8e6ac78d39a175b656

---
+++ benchmark-monitoring/data.js
@@ -0,0 +1,54 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1756876433374,
+  "repoUrl": "https://github.com/bugoutianzhen123/casbin-ucon",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "bugoutianzhen123",
+            "username": "bugoutianzhen123",
+            "email": "2804366305@qq.com"
+          },
+          "committer": {
+            "name": "bugoutianzhen123",
+            "username": "bugoutianzhen123",
+            "email": "2804366305@qq.com"
+          },
+          "id": "295ee996ce9e417ce7debd8e6ac78d39a175b656",
+          "message": "ci: temporarily disable automatic release",
+          "timestamp": "2025-09-03T01:14:44Z",
+          "url": "https://github.com/bugoutianzhen123/casbin-ucon/commit/295ee996ce9e417ce7debd8e6ac78d39a175b656"
+        },
+        "date": 1756876432965,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkPlaceholder",
+            "value": 0.3137,
+            "unit": "ns/op\t       0 B/op\t       0 allocs/op",
+            "extra": "1000000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkPlaceholder - ns/op",
+            "value": 0.3137,
+            "unit": "ns/op",
+            "extra": "1000000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkPlaceholder - B/op",
+            "value": 0,
+            "unit": "B/op",
+            "extra": "1000000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkPlaceholder - allocs/op",
+            "value": 0,
+            "unit": "allocs/op",
+            "extra": "1000000000 times\n4 procs"
+          }
+        ]
+      }
+    ]
+  }
+}
