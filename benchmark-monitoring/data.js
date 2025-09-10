7255375ef63ed6cd2680cd3cdd9e5e3239b9cf3e
github-action-benchmark <github@users.noreply.github.com>
2025-09-10 06:57:22 +0000

    add Benchmark (go) benchmark result for 58f0a87d929994ec342ef3cc4dee3eaa16a0e9c9

--- benchmark-monitoring/data.js
+++ benchmark-monitoring/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1757487016765,
+  "lastUpdate": 1757487442701,
   "repoUrl": "https://github.com/bugoutianzhen123/casbin-ucon",
   "entries": {
     "Benchmark": [
@@ -416,6 +416,54 @@ window.BENCHMARK_DATA = {
           {
             "name": "BenchmarkPlaceholder - ns/op",
             "value": 0.3136,
+            "unit": "ns/op",
+            "extra": "1000000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkPlaceholder - B/op",
+            "value": 0,
+            "unit": "B/op",
+            "extra": "1000000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkPlaceholder - allocs/op",
+            "value": 0,
+            "unit": "allocs/op",
+            "extra": "1000000000 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "2804366305@qq.com",
+            "name": "bugoutianzhen123",
+            "username": "bugoutianzhen123"
+          },
+          "committer": {
+            "email": "2804366305@qq.com",
+            "name": "bugoutianzhen123",
+            "username": "bugoutianzhen123"
+          },
+          "distinct": true,
+          "id": "58f0a87d929994ec342ef3cc4dee3eaa16a0e9c9",
+          "message": "feat: improve session manager and update README to highlight continuous authorization",
+          "timestamp": "2025-09-10T14:56:46+08:00",
+          "tree_id": "d1ae8300d24649c867b986c3afde643ef2d75497",
+          "url": "https://github.com/bugoutianzhen123/casbin-ucon/commit/58f0a87d929994ec342ef3cc4dee3eaa16a0e9c9"
+        },
+        "date": 1757487442353,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkPlaceholder",
+            "value": 0.3124,
+            "unit": "ns/op\t       0 B/op\t       0 allocs/op",
+            "extra": "1000000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkPlaceholder - ns/op",
+            "value": 0.3124,
             "unit": "ns/op",
             "extra": "1000000000 times\n4 procs"
           },
