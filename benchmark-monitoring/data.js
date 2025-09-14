278ac81af9d6eb5e534ae9b9ff33b70b86e8e98f
github-action-benchmark <github@users.noreply.github.com>
2025-09-14 07:25:53 +0000

    add Benchmark (go) benchmark result for 479467ecd31d68146a0b0c405cee4d45e3dfadd1

--- benchmark-monitoring/data.js
+++ benchmark-monitoring/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1757588331985,
+  "lastUpdate": 1757834753047,
   "repoUrl": "https://github.com/bugoutianzhen123/casbin-ucon",
   "entries": {
     "Benchmark": [
@@ -896,6 +896,54 @@ window.BENCHMARK_DATA = {
           {
             "name": "BenchmarkPlaceholder - ns/op",
             "value": 0.3115,
+            "unit": "ns/op",
+            "extra": "1000000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkPlaceholder - B/op",
+            "value": 0,
+            "unit": "B/op",
+            "extra": "1000000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkPlaceholder - allocs/op",
+            "value": 0,
+            "unit": "allocs/op",
+            "extra": "1000000000 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "hsluoyz@gmail.com",
+            "name": "Yang Luo",
+            "username": "hsluoyz"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "479467ecd31d68146a0b0c405cee4d45e3dfadd1",
+          "message": "Update ucon_enforcer_test.go\n\nCo-authored-by: Copilot <175728472+Copilot@users.noreply.github.com>",
+          "timestamp": "2025-09-14T15:25:32+08:00",
+          "tree_id": "06a52e5ee5fced85b192e80959233c50ef02ca5b",
+          "url": "https://github.com/bugoutianzhen123/casbin-ucon/commit/479467ecd31d68146a0b0c405cee4d45e3dfadd1"
+        },
+        "date": 1757834752465,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkPlaceholder",
+            "value": 0.3117,
+            "unit": "ns/op\t       0 B/op\t       0 allocs/op",
+            "extra": "1000000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkPlaceholder - ns/op",
+            "value": 0.3117,
             "unit": "ns/op",
             "extra": "1000000000 times\n4 procs"
           },
